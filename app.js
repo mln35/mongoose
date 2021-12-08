ad6956f9e0134647bd5c5267b3a870cca38fd0b2
mln35 <mouhametlatyr@gmail.com>
2021-12-08 14:49:06 +0000

    initial commit

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
-const createCourse = () => {
-    
+mongoose = require('mongoose');
+Course = require('./src/model/cours.model')
+
+const connecter = async() =>{
+    try{    
+        await mongoose.connect('mongodb://localhost:27017/bootcamp');
+        console.log('Connected to the database!');
+    }
+    catch(e){
+        console.log(e.message);
+    }
 }
+
+connecter();
+
+
+const creer = async(course)=>{
+    try{
+        await Course.create(course);
+    }
+    catch(e){
+        console.error(e.message);
+    }
+}
+creer({label:'JS',volume:70});
